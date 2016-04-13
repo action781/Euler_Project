08aaece244096716699023ea664c187b2452f85a
action781 <action781@gmail.com>
2016-04-13 00:43:15 -0400

    7 and 9

--- Problem7.R
+++ Problem7.R
@@ -2,10 +2,8 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 
 x <- NULL
-if()
-         x <- append(x, i)
+for (prime in 1:100000) {
+    if(sum(prime%%1:prime == 0)==2) #this tests if a number is prime
+            x <- append(x, prime) #this stores it primes in x
 }
 
-some number 27
-for i in i:sqrt(27)
-if a/i =0
